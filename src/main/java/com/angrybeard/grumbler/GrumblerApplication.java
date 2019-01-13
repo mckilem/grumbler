acd1c107e39b8e59734cf590964d3bd2a2518500
mckilem <andrianovgd@gmail.com>
2019-01-13 16:25:56 +0500

    initial commit

---
+++ src/main/java/com/angrybeard/grumbler/GrumblerApplication.java
@@ -0,0 +1,14 @@
+package com.angrybeard.grumbler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrumblerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GrumblerApplication.class, args);
+	}
+
+}
+
